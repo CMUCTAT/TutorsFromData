942b1df966c168ec96fda24dac7904f07c322da0
MaxDBenson <mbenson1994@gmail.com>
2021-10-05 10:31:35 -0400

    replay enactments

---
+++ HTML/Assets/tutorContainer.js
@@ -0,0 +1,32 @@
+(function() {
+	var bc = new BroadcastChannel('student_transactions');
+	var tutorFrame = document.getElementById('tutor_frame');
+	const urlParams = new URLSearchParams(window.location.search);
+	const myId = urlParams.get('tab_id');
+	bc.onmessage = function(msg) {
+		msg = msg.data;
+		if (msg.to === myId) {
+			switch(msg.type) {
+				case 'load':
+					tutorFrame.src = msg.data;
+				break;
+				case 'step': 
+					let ctatSAI = new CTATSAI(msg.data.selection, msg.data.action, msg.data.input),
+					stepType = "ATTEMPT";
+					console.log("\tsend sai: "+msg.data.selection+","+msg.data.action+","+msg.data.input+", ("+(msg.data.tutored ? "" : "un")+"tutored)");
+					tutorFrame.window.CTATCommShell.commShell.processComponentAction(
+																ctatSAI, //sai
+																msg.data.tutored, //tutored
+																true, //behavior recorded
+																null,  //[deprecated]
+																stepType, //log type
+																null, //aTrigger
+																msg.data.transactionID //transaction id
+																);
+				break;
+			}
+		}
+	}
+	
+	bc.postMessage({sender: myId, data: 'next problem'});
+})();
