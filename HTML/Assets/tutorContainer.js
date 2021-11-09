f6b2977420b2b403a62a39e93d6941a2dfc06cad
MaxDBenson <mbenson1994@gmail.com>
2021-11-09 13:08:15 -0500

    fix load event not always working

--- HTML/Assets/tutorContainer.js
+++ HTML/Assets/tutorContainer.js
@@ -1,4 +1,54 @@
 (function() {
+	
+	function iFrameReady(iFrame, fn) {
+		var timer;
+		var fired = false;
+
+		function ready() {
+			if (!fired) {
+				fired = true;
+				clearTimeout(timer);
+				fn.call(this);
+			}
+		}
+
+		function readyState() {
+			if (this.readyState === "interactive") {
+				ready.call(this);
+			}
+		}
+
+		// cross platform event handler for compatibility with older IE versions
+		function addEvent(elem, event, fn) {
+			if (elem.addEventListener) {
+				return elem.addEventListener(event, fn);
+			} else {
+				return elem.attachEvent("on" + event, function () {
+					return fn.call(elem, window.event);
+				});
+			}
+		}
+
+		function checkLoaded() {
+			var doc = iFrame.contentDocument || iFrame.contentWindow.document;
+			// We can tell if there is a dummy document installed because the dummy document
+			// will have an URL that starts with "about:".  The real document will not have that URL
+			if (doc.URL.indexOf("about:") !== 0) {
+				if (doc.readyState === "interactive") {
+					ready.call(doc);
+				} else {
+					// set event listener for DOMContentLoaded on the new document
+					addEvent(doc, "DOMContentLoaded", ready);
+					addEvent(doc, "readystatechange", readyState);
+				}
+			} else {
+				// still same old original document, so keep looking for content or new document
+				timer = setTimeout(checkLoaded, 1);
+			}
+		}
+		checkLoaded();
+	}
+	
 	var bc = new BroadcastChannel('student_transactions');
 	var tutorFrame = document.getElementById('tutor_frame');
 	const urlParams = new URLSearchParams(window.location.search);
@@ -13,7 +63,8 @@
 					bc.postMessage({sender: myId, type: "next problem"});
 				break;
 				case 'load':
-					tutorFrame.addEventListener("DOMContentLoaded", function(e) {
+					
+					iFrameReady(tutorFrame, function(e) {
 						console.log("tutorFrame DOMContentLoaded event");
 						if (tutorFrame.location.href !== "about:blank") {
 							tutorFrame.contentWindow.addEventListener("message", (e)=> {
@@ -28,6 +79,7 @@
 							});
 						}
 					});
+					
 					tutorFrame.src = msg.data;
 				break;
 				case 'step': 
