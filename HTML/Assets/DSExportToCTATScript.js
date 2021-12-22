4cc7223b9f6c7915c1eb5a8c1f4e37789dca79c5
MaxDBenson <mbenson1994@gmail.com>
2021-12-22 15:47:22 -0500

    set school to Default School if blank

--- HTML/Assets/DSExportToCTATScript.js
+++ HTML/Assets/DSExportToCTATScript.js
@@ -452,7 +452,7 @@ function openTutorInNewWindow(onloadFunc, isLocal) {
 		url = window.location.origin+"/run_replay_problem_set_as_assignment/"+problemData.packageName+"/"+problemData.problemSet+"/"+problemData.problemName;
 		let school = problemData.school || "Default School";
 		let classname = problemData.class || "Default Class";
-		let query = "?school_name="+school+"&class_name="+className+"&assignment_name="+problemData.assignment+"&student_name="+problemData.studentName+"&reset=false&first=false";
+		let query = "?school_name="+school+"&class_name="+classname+"&assignment_name="+problemData.assignment+"&student_name="+problemData.studentName+"&reset=false&first=false";
 		url+=query;
 		console.log("\t url (raw): ",url);
 		url = encodeURI(url);
